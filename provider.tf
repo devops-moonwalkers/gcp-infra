931d6958c306350ea5b620eb3dcc42f2556f9f5e
Nicolas Hostert <nicolas.hostert@mail.mcgill.ca>
2021-10-26 23:25:32 +0000

    Solving git version issues
    I had to fool around with git to make this work

--- provider.tf
+++ provider.tf
@@ -1,9 +1,12 @@
 terraform {
-  required_providers {
+  required_providers { 
     google = {
       source  = "hashicorp/google"
       version = "~> 3.70.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.70.0"
+    }
   }
 }
-
--- static_ip.tf
+++ static_ip.tf
@@ -1,7 +1,3 @@
-provider "google" {
-  project = var.gcp_project_id
-}
-
 resource "google_compute_address" "regional_external_ip" {
   provider      = google
   name          = "static-ingres-ip"
